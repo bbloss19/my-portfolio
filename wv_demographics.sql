a0c95d58273b6725bba13cd8185727b2363693c0
bbloss19 <bbloss@student.umgc.edu>
2025-02-07 13:24:02 -0500

    Update wv_demographics.sql

--- wv_demographics.sql
+++ wv_demographics.sql
@@ -1,3 +1,32 @@
+--Takes average income from each region in PA, WV, and MD, and then classifies it as either wealthy, average, or poor.
+SELECT DISTINCT region, state, ROUND(AVG(median_income), 0) AS avg_income,
+CASE
+	WHEN AVG(median_income) > 90000 THEN 'Wealthy'
+	WHEN AVG(median_income) < 60000 THEN 'poor'
+	ELSE 'Average'
+END AS 'classification'
+FROM pennsylvania
+GROUP BY state, region
+UNION ALL
+SELECT DISTINCT region, state, ROUND(AVG(median_income), 0) AS avg_income,
+CASE
+	WHEN AVG(median_income) > 90000 THEN 'Wealthy'
+	WHEN AVG(median_income) < 60000 THEN 'poor'
+	ELSE 'Average'
+END AS 'classification'
+FROM wv_demographics
+GROUP BY state, region
+UNION ALL
+SELECT DISTINCT region, state, ROUND(AVG(median_income), 0) AS avg_income,
+CASE
+	WHEN AVG(median_income) > 90000 THEN 'Wealthy'
+	WHEN AVG(median_income) < 60000 THEN 'poor'
+	ELSE 'Average'
+END AS 'classification'
+FROM maryland
+GROUP BY state, region
+ORDER BY avg_income DESC;
+
 --Compares the pecent increase median income in WV counties from 2010 to 2022 
 SELECT wv.county, 
 wv.median_income AS median_income_2022, 
